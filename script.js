2116fef7f37cbd4eae63505c00c049be3de6062b
Chiiorrma <chiumuo76@gmail.com>
2023-12-20 03:10:44 -0800

    Adding the functionality for the page

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", performAction());
+
+function performAction() {
+  let myForm = document.getElementById("myForm");
+  let greetingText = document.getElementById("greeting");
+  let clearName = document.getElementById("clearName");
+
+  //Checks if the user's name is stored on page load
+  const savedName = localStorage.getItem("userName");
+  if (savedName) {
+    greetUser(savedName);
+  };
+
+  //Event listener for form submission.
+  myForm.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let userNameInput = document.getElementById('name');
+    let userName = userNameInput.value.trim();
+
+    if (userName !== '') {
+      localStorage.setItem('userName', userName);
+      greetUser(userName);
+    }
+  };
+  //Event listener to clear saved name
+  clearName.addEventListener('click', performClear);
+
+  function performClear() {
+    localStorage.clear('userName');
+    greetUser(null);
+  };
+
+  function greetUser(name) {
+    if (name) {
+      greetingText.innerHTML = `Welcome ${name}! It's nice to meet you😊.`;
+    } else {
+      greetingText.innerHTML = `Hello there👋, Please enter your name.`
+    }
+  }
+};
